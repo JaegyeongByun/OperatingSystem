1ce43e0c7aa9a1bc5e30823b2358ed8c3f948d19
JaegyeongByun <worud1208@gmail.com>
2023-05-29 19:31:06 +0900

    ADD main.c

---
+++ C Tutorial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  
+//
+//  Created by 변재경 on 2023/05/29.
+//
+
+#include "main.h"
+
+int main()
+{
+    printf("hello, world!");
+    
+    return 0;
+}
---
+++ C Tutorial/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  
+//
+//  Created by 변재경 on 2023/05/29.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
